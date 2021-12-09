3411c4463b237aa9e665bf219d68033df1808601
CinqueIzumi <ba.peereboom@gmail.com>
2021-12-09 16:35:08 +0100

    [Added] Simple LCD support

--- main/main.c
+++ main/main.c
@@ -2,20 +2,86 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "rom/uart.h"
 
-void hello_task(void *pvParameter)
+#include "smbus.h"
+#include "i2c-lcd1602.h"
+
+#define TAG "app"
+
+// LCD1602
+#define LCD_NUM_ROWS               2
+#define LCD_NUM_COLUMNS            32
+#define LCD_NUM_VISIBLE_COLUMNS    16
+
+// Undefine USE_STDIN if no stdin is available (e.g. no USB UART) - a fixed delay will occur instead of a wait for a keypress.
+#define USE_STDIN  1
+//#undef USE_STDIN
+
+#define I2C_MASTER_NUM           I2C_NUM_0
+#define I2C_MASTER_TX_BUF_LEN    0                     // disabled
+#define I2C_MASTER_RX_BUF_LEN    0                     // disabled
+#define I2C_MASTER_FREQ_HZ       100000
+#define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
+#define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
+
+static i2c_lcd1602_info_t* lcd_info;
+
+static void init(void)
 {
-    printf("Hello world!\n");
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    // Set up I2C
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_MASTER_RX_BUF_LEN,
+                       I2C_MASTER_TX_BUF_LEN, 0);
+
+    i2c_port_t i2c_num = I2C_MASTER_NUM;
+    uint8_t address = CONFIG_LCD1602_I2C_ADDRESS;
+
+    // Set up the SMBus
+    smbus_info_t * smbus_info = smbus_malloc();
+    ESP_ERROR_CHECK(smbus_init(smbus_info, i2c_num, address));
+    ESP_ERROR_CHECK(smbus_set_timeout(smbus_info, 1000 / portTICK_RATE_MS));
+
+    // Set up the LCD1602 device with backlight off
+    lcd_info = i2c_lcd1602_malloc();
+    ESP_ERROR_CHECK(i2c_lcd1602_init(lcd_info, smbus_info, true,
+                                     LCD_NUM_ROWS, LCD_NUM_COLUMNS, LCD_NUM_VISIBLE_COLUMNS));
+
+    ESP_ERROR_CHECK(i2c_lcd1602_reset(lcd_info));
+}
+
+void screen_temperature_task(void * pvParameter) 
+{
+
+}
+
+void lcd1602_task(void * pvParameter)
+{
+    ESP_LOGI(TAG, "Printing string to lcd");
+    i2c_lcd1602_clear           (lcd_info);
+    i2c_lcd1602_set_cursor      (lcd_info, true);
+    i2c_lcd1602_move_cursor     (lcd_info, 0, 0);
+    i2c_lcd1602_write_string    (lcd_info, "Hello world!");
+
+    vTaskDelete(NULL);
 }
 
 void app_main()
 {
-    xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+    init();
+
+    xTaskCreate(&lcd1602_task, "lcd1602_task", 4096, NULL, 5, NULL);
 }
