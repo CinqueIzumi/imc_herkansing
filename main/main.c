70548df3653ef59d1ce3fc845346ea325984cdd3
CinqueIzumi <ba.peereboom@gmail.com>
2022-01-18 20:45:18 +0100

    [Added] Implemented relay within application

--- main/main.c
+++ main/main.c
@@ -55,6 +55,7 @@ struct LcdData
     int current_humidity;
     int preferred_temp;
     int16_t current_screen;
+    int thermostat;
 } xLcdData;
 
 /* Ensure that the struct can be passed by value */
@@ -78,27 +79,7 @@ void update_dht_data();
 void rotary_encoder_on_clicked();
 void rotary_encoder_on_moved(int16_t idx);
 static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
-
-void relay_task(void *pvParameter)
-{
-    /* Configure the IOMUX register for pad RELAY_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_pad_select_gpio(RELAY_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(RELAY_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Relay off (output low) */
-        gpio_set_level(RELAY_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Relay on (output high) */
-        gpio_set_level(RELAY_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
+void control_thermostat(int control);
 
 /* The main function */
 void app_main()
@@ -166,7 +147,6 @@ void app_main()
 
     /* Initialize the LCD screen task */
     xTaskCreate(lcd_screen_task, "lcd_screen_task", 1024*2, (void*)0, 10, &active_task_handle);
-    xTaskCreate(relay_task, "relay_task", 1024*2, NULL, 5, NULL);
 
     /* Add the initialized struct to the queue, to show the current temperature on boot */
     xQueueSend(xStructQueue, (void *) &xLcdData, ( TickType_t ) 0);
@@ -183,10 +163,11 @@ void app_main()
 void initialize_queues(void)
 {
     /* Set the default data of the LCD data struct */
-    xLcdData.current_humidity = 0;
-    xLcdData.current_temp = 0;
+    xLcdData.current_humidity = -1;
+    xLcdData.current_temp = -1;
     xLcdData.preferred_temp = 18;
     xLcdData.current_screen = 0x00;
+    xLcdData.thermostat = 0;
 
     /* Initialize the queue */
     xStructQueue = xQueueCreate(
@@ -228,6 +209,9 @@ void lcd_screen_task(void *pvParameters)
                     draw_humidity_screen(xReceivedStructure);
                     break;
             }
+
+            /* Update the thermostat, based on the given data */
+            control_thermostat(xReceivedStructure.thermostat);
         }
     }
 
@@ -303,6 +287,15 @@ void rotary_encoder_on_clicked()
 
     /* Update the struct which holds the data */
     update_dht_data();
+
+    if(xLcdData.preferred_temp > xLcdData.current_temp)
+    {
+        xLcdData.thermostat = 1;
+    }
+    else
+    {
+        xLcdData.thermostat = 0;                
+    }
 
     /* Send the entire struct to the queue */
     xQueueSend(
@@ -398,6 +391,15 @@ static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_ser
             default:
                 break;
         }
+
+        if(xLcdData.preferred_temp > xLcdData.current_temp)
+        {
+            xLcdData.thermostat = 1;
+        }
+        else
+        {
+            xLcdData.thermostat = 0;      
+        }
         /* Add the modified lcd data to the queue */
         xQueueSend(xStructQueue, (void *) &xLcdData, ( TickType_t ) 0);
 
@@ -406,4 +408,22 @@ static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_ser
     }
 
     return ESP_OK;
+}
+
+/* A function which can be used to control the thermostat */
+void control_thermostat(int control)
+{
+    /* 
+        Flips the 1 and 0
+        The relay uses inverted values, so 0 would turn on the led, and 1 would turn off the led
+    */
+    int status = 1 - control;
+
+    gpio_pad_select_gpio(RELAY_GPIO);
+    
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(RELAY_GPIO, GPIO_MODE_OUTPUT);
+
+    gpio_set_level(RELAY_GPIO, status);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
