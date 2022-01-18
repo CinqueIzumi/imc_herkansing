8b6c34a55a829beeac5c1d86f6ebfaaf6a68526d
CinqueIzumi <ba.peereboom@gmail.com>
2022-01-18 17:09:15 +0100

    [Modified] Updated '//' comments to '/**/' comments

--- main/main.c
+++ main/main.c
@@ -8,37 +8,38 @@
 #include "sdkconfig.h"
 #include "rom/uart.h"
 
-// Used for the LCD
+/* Used for the LCD */
 #include "smbus.h"
 #include "i2c-lcd1602.h"
 
-// Used for the rotary encoder
+/* Used for the rotary encoder */
 #include "qwiic_twist.h"
 
-// Used for the temp sensor
+/* Used for the temp sensor */
 #include "dht11.h"
 
-// Used for the on-board buttons
+/* Used for the on-board buttons */
 #include "esp_peripherals.h"
 #include "periph_adc_button.h"
 #include "input_key_service.h"
 
+/* Define the tag, which will be used for logging */
 #define TAG "imc_herkansing"
 
-// LCD1602
+/* LCD1602 */
 #define LCD_NUM_ROWS               3
 #define LCD_NUM_COLUMNS            20
 #define LCD_NUM_VISIBLE_COLUMNS    20
 
-// I2C
+/* I2C */
 #define I2C_MASTER_NUM           I2C_NUM_0
-#define I2C_MASTER_TX_BUF_LEN    0                     // disabled
-#define I2C_MASTER_RX_BUF_LEN    0                     // disabled
+#define I2C_MASTER_TX_BUF_LEN    0                     /* disabled */
+#define I2C_MASTER_RX_BUF_LEN    0                     /* disabled */
 #define I2C_MASTER_FREQ_HZ       100000
 #define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
 #define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
 
-// Variables used to keep track of the global components
+/* Variables used to keep track of the global components */
 static i2c_lcd1602_info_t* lcd_info;
 static qwiic_twist_t* qwiic_handle;
 
@@ -53,21 +54,22 @@ struct LcdData
     int preferred_temp;
 } xLcdData;
 
-// Variables used to keep track of the current information
+/* Variables used to keep track of the current information */
 int current_temp = 24;
 int pref_temp = 20;
 int current_hum = 30;
 
+/* Variables used to keep track of the current states */
 static int16_t screen_current_state = 0x00; 
 static TaskHandle_t active_task_handle;
 
-// Used to determine and control the LCD screen state
+/* Used to determine and control the LCD screen state */
 enum screen_state {
     SCREEN_TEMP = 0x00,
     SCREEN_HUM = 0x01,
 };
 
-// Function declarations due to there not being a header file
+/* Function declarations due to there not being a header file */
 static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
 void rotary_encoder_on_moved(int16_t idx);
 void rotary_encoder_on_clicked();
@@ -76,19 +78,21 @@ void switch_screen_states(int16_t newState);
 void screen_humidity_task();
 void screen_temperature_task();
 
-// Testing
+/* Testing */
 void initialize_queues(void);
 void screen_temperature_task_simplified(void *pvParameters);
 void update_sensor_struct(void);
 void on_rotary_clicked_simplified();
 
-
+/* A function which initialized the queue, which can be used to communicatie between tasks */
 void initialize_queues(void)
 {
+    /* Set the default data of the LCD data struct */
     xLcdData.current_humidity = 0;
     xLcdData.current_temp = 0;
     xLcdData.preferred_temp = 18;
 
+    /* Initialize the queue */
     xStructQueue = xQueueCreate(
         /* The maximum number of items the queue can hold*/
         5,
@@ -96,6 +100,7 @@ void initialize_queues(void)
         sizeof( xLcdData )
     );
 
+    /* Check to determine whether the queue has been initialized successfully */
     if(xStructQueue == NULL)
     {
         ESP_LOGE(TAG, "Queue has not been initialized successfully");
@@ -109,9 +114,9 @@ void app_main()
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
     conf.sda_io_num = I2C_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;  /* GY-2561 provides 10kΩ pullups */
     conf.scl_io_num = I2C_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;  /* GY-2561 provides 10kΩ pullups */
     conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
     i2c_param_config(i2c_master_port, &conf);
     i2c_driver_install(i2c_master_port, conf.mode,
@@ -168,11 +173,10 @@ void app_main()
 
     ESP_LOGI(TAG, "[ 9 ] Waiting for a button to be pressed...");
 
-    // Set the start screen to the temp screen
-    // switch_screen_states(0x00);
+    /* Set the start screen to the temp screen */
     xTaskCreate(screen_temperature_task_simplified, "screen_temperature_task_simplified", 1024*2, (void*)0, 10, &active_task_handle);
 
-    // Initialize the rotary encoder
+    /* Initialize the rotary encoder */
     if (qwiic_twist_start_task(qwiic_config) != ESP_OK)
     {
         ESP_LOGE(TAG, "TASK_START_QWIIC_FAILED");
@@ -180,16 +184,17 @@ void app_main()
     }
 }
 
+/* A function which draws and maintains the temperature screen */
 void screen_temperature_task() 
 {
-    // Convert the global int variables to strings
+    /* Convert the global int variables to strings */
     char snum_current_temp[2];
     sprintf(snum_current_temp, "%d", current_temp);
 
     char snum_pref_temp[2];
     sprintf(snum_pref_temp, "%d", pref_temp);
 
-    // Display the temperature screen on the lcd 
+    /* Display the temperature screen on the lcd  */
     ESP_LOGI(TAG, "Displaying temperature screen");
     i2c_lcd1602_clear           (lcd_info);
 
@@ -210,46 +215,60 @@ void screen_temperature_task()
     vTaskDelete(NULL);
 }
 
+/* A function which draws and maintains the temperature screen */
 void screen_temperature_task_simplified(void *pvParameters)
 {
+    /* Initialize the counter integer */
     int counter = 0;
+
+    /* Tasks should never return, to prevent starvation, event-based tasks are recommended */
     for(; ;)
     {
+        /* Initialize the received structure struct */
         struct LcdData xReceivedStructure;
 
+        /* Check whether there's any new events in the queue */
         BaseType_t result;
         result = xQueueReceive(xStructQueue, &xReceivedStructure, ( TickType_t ) 10);
 
         if(result == pdPASS)
         {
+            /* Update the counter */
             counter = counter + 1;
 
+            /* Convert the counter integer to a char array */
             char snum_current_counter[12];
             sprintf(snum_current_counter, "%d", counter);
 
+            /* Clear the LCD and write the counter value */
             i2c_lcd1602_clear           (lcd_info);
             i2c_lcd1602_write_string    (lcd_info, snum_current_counter);
         }
     }
 
+    /* If the task does happen to exist, ensure that the exit is a clean one */
     vTaskDelete(NULL);
 }
 
+/* A function which will update the sensor struct, to keep track of the current values */
 void update_sensor_struct(void)
 {
+    /* Update the struct, based on the DHT values */
     xLcdData.current_temp = DHT11_read().temperature;
     xLcdData.current_humidity = DHT11_read().humidity;
 
-    // Log the results in the console
+    /* Log the results in the console */
     printf("Temperature is %d \n", xLcdData.current_temp);
     printf("Humidity is %d\n", xLcdData.current_humidity);
     ESP_LOGI(TAG, "Data has been updated");
 }
 
+/* A function which executes when the rotary encoder has been clicked */
 void on_rotary_clicked_simplified()
 {
     ESP_LOGI(TAG, "Rotary encoder has been clicked!");
-    // Update the struct which holds the data
+
+    /* Update the struct which holds the data */
     update_sensor_struct();
 
     /* Send the entire struct to the queue */
@@ -265,13 +284,14 @@ void on_rotary_clicked_simplified()
     );
 } 
 
+/* A function which draws and maintains the humidity screen */
 void screen_humidity_task() 
 {
-    // Convert the global int variables to strings
+    /* Convert the global int variables to strings */
     char snum_current_hum[2];
     sprintf(snum_current_hum, "%d", current_hum);
 
-    // Display the humidity screen on the lcd 
+    /* Display the humidity screen on the lcd  */
     ESP_LOGI(TAG, "Displaying humidity screen");
     i2c_lcd1602_clear           (lcd_info);
 
@@ -285,61 +305,63 @@ void screen_humidity_task()
     vTaskDelete(NULL);
 }
 
+/* A function which manages switching between the separate screens */
 void switch_screen_states(int16_t newState)
 {
-    // Create a task to display the correct screen, based on the given screen state
+    /* Create a task to display the correct screen, based on the given screen state */
     switch(newState) 
     {
         case SCREEN_TEMP:
             xTaskCreate(screen_temperature_task, "screen_temp_task", 1024*2, (void*)0, 10, &active_task_handle);
-            screen_current_state = newState;
             break;
         case SCREEN_HUM:
             xTaskCreate(screen_humidity_task, "screen_hum_task", 1024*2, (void*)0, 10, &active_task_handle);
-            screen_current_state = newState;
             break;
     }
 
-    // Update the current screen state to keep track of the current screen
+    /* Update the current screen state to keep track of the current screen */
     screen_current_state = newState;
 }
 
+/* A function which updates the DHT data */
 void update_dht_data()
 {
-    // Read and update the current temperature and humidity
+    /* Read and update the current temperature and humidity */
     current_temp = DHT11_read().temperature;
     current_hum = DHT11_read().humidity;
 
-    // Log the results in the console
+    /* Log the results in the console */
     printf("Temperature is %d \n", DHT11_read().temperature);
     printf("Humidity is %d\n", DHT11_read().humidity);
     ESP_LOGI(TAG, "Data has been updated");
 
-    // Refresh the menu displayed on the lcd, to display the up-to-date values
+    /* Refresh the menu displayed on the lcd, to display the up-to-date values */
     switch_screen_states(screen_current_state);
 
     vTaskDelete(NULL);
 }
 
+/* A function which handles the execution when the rotary encoder has been clicked */
 void rotary_encoder_on_clicked()
 {
-    // Update the dht sensor data
+    /* Update the dht sensor data */
     xTaskCreate(update_dht_data, "update_dht_data", 2048, NULL, 10, NULL);
     vTaskDelay(1000 / portTICK_RATE_MS);
 }
 
+/* A function which handles the execution when the rotary encoder has been moved */
 void rotary_encoder_on_moved(int16_t idx)
 { 
-    // Switch between screens based on the direction the rotary encoder has been rotated in
+    /* Switch between screens based on the direction the rotary encoder has been rotated in */
     if(idx == 1)
     {
-        // Rotary encoder has been moved counter-clockwise
+        /* Rotary encoder has been moved counter-clockwise */
         screen_current_state = 0x01;
         switch_screen_states(0x01);
     } 
     else if (idx == -1)
     {
-        // Rotary encoder has been moved clockwise
+        /* Rotary encoder has been moved clockwise*/
         screen_current_state = 0x00;
         switch_screen_states(0x00);
     }
@@ -347,16 +369,17 @@ void rotary_encoder_on_moved(int16_t idx)
     vTaskDelay(1000 / portTICK_RATE_MS);
 }
 
+/* A function which handles the on-board key inputs */
 static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx)
 {
     const int input_type = evt->type;
 
     if(input_type == 1)
     {
-        // Handle the pressed button accordingly by checking what button had been clicked
+        /* Handle the pressed button accordingly by checking what button had been clicked */
         switch ((int)evt->data) {
             case INPUT_KEY_USER_ID_SET:
-                // Navigate to the currently non-active screen
+                /* Navigate to the currently non-active screen */
                 if(screen_current_state == 0x00)
                 {
                     switch_screen_states(0x01); 
@@ -366,7 +389,7 @@ static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_ser
                 }
                 break;
             case INPUT_KEY_USER_ID_VOLDOWN:
-                // Only reduce the preffered temp if the user is in the temperature screen, and the preffered temperature is above 0
+                /* Only reduce the preffered temp if the user is in the temperature screen, and the preffered temperature is above 0 */
                 if(screen_current_state == 0x00)
                 {
                     if(pref_temp > 0)
@@ -378,11 +401,11 @@ static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_ser
                 }
                 break;
             case INPUT_KEY_USER_ID_PLAY:
-                // Create a task to update the sensor data
+                /* Create a task to update the sensor data */
                 xTaskCreate(update_dht_data, "update_dht_data", 2048, NULL, 10, NULL);
                 break;
             case INPUT_KEY_USER_ID_VOLUP:
-                // Only increase the preffered temp if the user is in the temperature screen, and the preffered temperature is below 30
+                /* Only increase the preffered temp if the user is in the temperature screen, and the preffered temperature is below 30 */
                 if(screen_current_state == 0x00)
                 {
                     if(pref_temp < 30)
